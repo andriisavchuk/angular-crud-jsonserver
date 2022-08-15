cb30f4df27659fc09adfd1c48e3aac069729f421
Andrii Savchuk <andrii.savchuk@bondbrandloyalty.com>
2022-08-15 14:24:36 -0400

    added form template

--- src/app/add-item-dialog/add-item-dialog.component.ts
+++ src/app/add-item-dialog/add-item-dialog.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-add-item-dialog',
   templateUrl: './add-item-dialog.component.html',
-  styleUrls: ['./add-item-dialog.component.css']
+  styleUrls: ['./add-item-dialog.component.css'],
 })
 export class AddItemDialogComponent implements OnInit {
+  itemsCategory = [
+    { category: 'Appliances' },
+    { category: 'Electronics' },
+    { category: 'Electrical' },
+    { category: 'Building Materials' },
+  ];
+  itemStatuses: string[] = ['New', 'Pre-Owned', 'Used'];
+  selectedStatus: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { AddItemDialogComponent } from './add-item-dialog/add-item-dialog.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatRadioModule } from '@angular/material/radio';
 
 @NgModule({
   declarations: [AppComponent, AddItemDialogComponent],
@@ -17,10 +24,18 @@ import { AddItemDialogComponent } from './add-item-dialog/add-item-dialog.compon
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatDialogModule
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRadioModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent],
