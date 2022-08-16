b0ebeec01952ecab2f67bfcffc904565cbd3593a
Andrii Savchuk <andrii.savchuk@bondbrandloyalty.com>
2022-08-16 14:31:19 -0400

    added getAllItems method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { AddItemDialogComponent } from './add-item-dialog/add-item-dialog.component';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  constructor(private dialog: MatDialog) {}
+export class AppComponent implements OnInit {
+  constructor(private dialog: MatDialog, private api: ApiService) {}
+
+  ngOnInit() {
+    this.getAllItems();
+  }
 
   openAddItemDialog() {
     const dialogRef = this.dialog.open(AddItemDialogComponent, {
@@ -17,6 +22,17 @@ export class AppComponent {
 
     dialogRef.afterClosed().subscribe((result) => {
       console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  getAllItems() {
+    this.api.getItemsFromList().subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+      error: (err) => {
+        alert('Error while fetching the Items for Data Base');
+      },
     });
   }
 }
